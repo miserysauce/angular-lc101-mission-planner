5f5bc772486ad97885dddc57fbdc473a04ff1079
AJT <lunegeoid@gmail.com>
2023-05-10 19:43:43 -0500

    updated with part 2 solutions

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
-
+ 
   inCrew: boolean = false;
   crew: object[] = [];
 
@@ -25,5 +25,30 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
+
+  addCrewMember(candidate: object){
+    if (!this.crew.includes(candidate) && this.crew.length < 3){
+      this.crew.push(candidate);
+    } else if (this.crew.includes(candidate)){
+     let index = this.crew.indexOf(candidate);
+     this.crew.splice(index, 1);
+
+    }
+  }
+
+  // addCrewMember(candidate: object){
+  //   if (this.crew.includes(candidate)){
+  //   this.inCrew = true;
+  //   }else{
+  //     this.inCrew = false;
+  //   }
+    
+  //   if (this.inCrew){
+  //     let index = this.crew.indexOf(candidate);
+  //     this.crew.splice(index, 1);
+  //   } else if(!this.inCrew && this.crew.length < 3){
+  //     this.crew.push(candidate);
+  //   }
+  // }
 
 }
